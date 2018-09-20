8508d3872475779d8892ac3409ce9eef7d805a4b
lass5643 <lass5643@stud.kea.dk>
2018-09-20 15:21:09 +0200

    Database setup
    Table created aswell as initial setup.
    
    Also added hardcoded insertions.

---
+++ db_scripts/Insertions.sql
@@ -0,0 +1,8 @@
+INSERT INTO Activity (name, age_restriction, description) 
+	VALUES
+		('Gocart', 16, 'This is gocart and it goes fast!'),
+		('Minigolf', 4, 'This is minigolf and it goes slow'),
+		('Paintball', 18, 'This is a shooting game, be aware!'),
+		('Sumo wrestling', 8, 'Fight like a fat guy!');
+
+SELECT * FROM Activity;
