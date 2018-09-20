148d817cdbc46be7754921eede87c8fb58fe5478
AndreasJepsenNielsen <32238389+AndreasJepsenNielsen@users.noreply.github.com>
2018-09-20 15:22:12 +0200

    dasd

--- src/main/java/com/example/demo/Model/Activity.java
+++ src/main/java/com/example/demo/Model/Activity.java
@@ -1,4 +1,68 @@
 package com.example.demo.Model;
 
-public class Activity {
+public class Activity
+{
+
+    //Fields
+    private int activityId;
+    private int ageRestriction;
+    private String name;
+    private String description;
+
+    //Constructor
+    public Activity(int activityId, int ageRestriction, String name, String description)
+    {
+        this.activityId = activityId;
+        this.ageRestriction = ageRestriction;
+        this.name = name;
+        this.description = description;
+    }
+
+    //No arg constructor
+    public Activity()
+    {
+
+    }
+
+    //Getters and Setters
+    public int getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+
+    public int getAgeRestriction() {
+        return ageRestriction;
+    }
+
+    public void setAgeRestriction(int ageRestriction) {
+        this.ageRestriction = ageRestriction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Activity:" +
+                "ageRestriction: " + ageRestriction +
+                ", name: '" + name + '\'' +
+                ", description: '" + description + '\'';
+    }
 }
