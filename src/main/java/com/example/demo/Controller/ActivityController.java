aac79cb57e0e95983b4a46e2411e1d01bdc65efb
Aske <aske.funder@gmail.com>
2018-09-25 11:34:30 +0200

    Update
    Tilføjet update GET og POST, samt tilføjet getOnId til ActivityRepository

--- src/main/java/com/example/demo/Controller/ActivityController.java
+++ src/main/java/com/example/demo/Controller/ActivityController.java
@@ -5,8 +5,10 @@ import com.example.demo.Repository.ActivityRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ActivityController
@@ -17,11 +19,11 @@ public class ActivityController
     @RequestMapping(value = "/CreateActivity", method = RequestMethod.GET)
     public String create (Model model)
     {
-        model.addAttribute("ActivityModel", new ActivityModel());
+        model.addAttribute("activitymodel", new ActivityModel());
         return "ActivityCreate";
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST  )
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String postActivity(@ModelAttribute ActivityModel activityModel)
     {
         activityRepository.createActivity(activityModel);
@@ -29,7 +31,27 @@ public class ActivityController
         return "redirect:/created/";
     }
 
-    
+    //Value og return skal udfyldes
+    //Value skal beholde /{id}, altså er det
+    //... der skal erstattes med et link
+    @RequestMapping(value = "/.../{id}", method = RequestMethod.GET)
+    public String updateActivity(@PathVariable int id, Model model)
+    {
+        ActivityModel activityModel = activityRepository.getOnId(id);
+        model.addAttribute("activitymodel", activityModel);
+
+        return "";
+    }
+
+    //Value og return skal udfyldes, når adressen er kendt
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String updateActivity(@ModelAttribute ActivityModel activityModel, RedirectAttributes rdt)
+    {
+        rdt.addFlashAttribute("message", "Aktivetet opdateret");
+        activityRepository.updateActivity(activityModel);
+
+        return "";
+    }
 
 
 }
--- src/main/java/com/example/demo/Repository/ActivityRepository.java
+++ src/main/java/com/example/demo/Repository/ActivityRepository.java
@@ -2,13 +2,18 @@ package com.example.demo.Repository;
 
 import com.example.demo.Model.ActivityModel;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ActivityRepository
 {
     private JdbcTemplate jdbc;
 
+    //Takes an activity model, caches it's values
+    //and inserts a new row with the cached values
     public void createActivity(ActivityModel activity)
     {
         int id = activity.getId();
@@ -18,5 +23,39 @@ public class ActivityRepository
 
         jdbc.update("INSERT INTO Activity (id, name, description, age_restriction)\n" +
                 "VALUES (" + id + ", " + name + ", " + description + ", " + ageRestriction + ");");
+    }
+
+    //Takes an activity model, caches it's values
+    //and updates an activity row with the values
+    //based on id
+    public void updateActivity(ActivityModel activityModel)
+    {
+        int ageRestriction = activityModel.getAgeRestriction();
+        String description = activityModel.getDescription();
+        int id = activityModel.getId();
+        String name = activityModel.getName();
+
+        String sql = "UPDATE Activity as a " +
+                "SET age_restriction = " + ageRestriction + ", description = " + description + ", name = " + name + " " +
+                "WHERE a.id = " + id + ";";
+
+        jdbc.update(sql);
+    }
+
+    //Creates an activity model from a SqlRowSet
+    //based on id, and return a model based on the values
+    //of the SqlRowSet
+    public ActivityModel getOnId(int id)
+    {
+        String sql = "SELECT * FROM Activity as a" +
+                "WHERE a.id = " + id + ";";
+        SqlRowSet rs = jdbc.queryForRowSet(sql);
+
+        int ageRestriction = rs.getInt("age_restriction");
+        String description = rs.getString("description");
+        String name = rs.getString("name");
+
+        ActivityModel activityModel = new ActivityModel(id, ageRestriction, name, description);
+        return activityModel;
     }
 }
