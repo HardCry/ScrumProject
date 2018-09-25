7439a6683e6d672d21832710ce4272f64fedb7a4
Aske <aske.funder@gmail.com>
2018-09-25 09:30:48 +0200

    RequestMapping
    RequestMapping tilføjet

--- src/main/java/com/example/demo/Controller/ActivityController.java
+++ src/main/java/com/example/demo/Controller/ActivityController.java
@@ -1,10 +1,14 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Activity;
 import com.example.demo.Repository.ActivityRepository;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ActivityController
@@ -12,8 +16,9 @@ public class ActivityController
     ActivityRepository activityRepository;
 
 
-    public void createActivity()
+    @RequestMapping(value = "/create", method = RequestMethod.POST  )
+    public void postActivity(@ModelAttribute Activity model)
     {
-        activityRepository.createActivity();
+        activityRepository.createActivity(model);
     }
 }
